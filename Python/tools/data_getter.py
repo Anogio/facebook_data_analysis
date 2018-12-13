f4f104b0095b611887349944d9997244783ec21f
Anogio <antoine.ogier@gmail.com>
2018-12-13 22:44:23 +0100

    Make functions fo retrieve data from .json

---
+++ Python/tools/data_getter.py
@@ -0,0 +1,19 @@
+import json
+import basemap
+import pillow
+from Python.tools.helpers import resolve_path
+
+DATA_FOLDER = 'Data/'
+
+IP_FILE = 'security_and_login_information/used_ip_addresses.json'
+
+
+def get_file(file):
+    with open(resolve_path(DATA_FOLDER + file), 'r') as file:
+        return json.load(file)
+
+
+def get_ips_list():
+    ips = get_file(IP_FILE)
+
+    return [entry['ip'] for entry in ips['used_ip_address']]
