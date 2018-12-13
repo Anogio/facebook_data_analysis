a7afb022be2c2becde566f8444d1c7418c3fbbb6
Anogio <antoine.ogier@gmail.com>
2018-12-13 23:23:41 +0100

    Get all geographical data on ips

--- Python/ip_locations/ip_map.py
+++ Python/ip_locations/ip_map.py
@@ -1,3 +1,9 @@
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
-import numpy as np
+from Python.ip_locations.ip_location_tools import get_ips_info_using_api
+from Python.tools.data_getter import get_ips_list
+
+
+def get_all_coordinates():
+    ips = get_ips_list()
+    print('Getting location info for {} IP addresses'.format(len(ips)))
+    return get_ips_info_using_api(ips, ['lon', 'lat'])
+
