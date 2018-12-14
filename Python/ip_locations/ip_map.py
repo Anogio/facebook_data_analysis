d249f435a10aafa2c74018619fe5448432f91596
Anogio <antoine.ogier@gmail.com>
2018-12-15 00:43:47 +0100

    Create map of conexion locations

--- Python/ip_locations/ip_map.py
+++ Python/ip_locations/ip_map.py
@@ -1,4 +1,37 @@
-from Python.ip_locations.ip_location_tools import get_ips_info_using_api
+import plotly.offline as py
 
 
+def make_map(coordinates_df):
+    data = [dict(
+        type='scattergeo',
+        lon=coordinates_df['lon'],
+        lat=coordinates_df['lat'],
+        text=coordinates_df['city'] + ', ' + coordinates_df['country'],
+        mode='markers',
+        marker=dict(
+            size=8,
+            opacity=0.8,
+            reversescale=True,
+            autocolorscale=False,
+            symbol='circle',
+            line=dict(
+                width=1,
+                color='rgba(102, 102, 102)'
+            ),
+        ))]
 
+    layout = dict(
+        title='Connexion locations based on IP addresses',
+        geo=dict(
+            scope='world',
+            projection=dict(type='winkel tripel'),
+            showcountries=True,
+            showland=True,
+            landcolor="rgb(250, 250, 250)",
+            subunitcolor="rgb(217, 217, 217)",
+            countrycolor="rgb(217, 217, 217)",
+        ),
+    )
+
+    fig = dict(data=data, layout=layout)
+    py.plot(fig, filename='ip_locations.html')
