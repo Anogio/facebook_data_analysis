725c3c79c7e98afcbc84fd33b2421a194749de97
Anogio <antoine.ogier@gmail.com>
2018-12-15 00:04:23 +0100

    Move coordinate getter to tools file

--- Python/ip_locations/ip_location_tools.py
+++ Python/ip_locations/ip_location_tools.py
@@ -1,6 +1,7 @@
 import requests
 from tqdm import tqdm
 
+from Python.tools.data_getter import get_ips_list
 from Python.tools.helpers import generate_sublists
 
 API_URL = 'http://ip-api.com/batch'
@@ -14,6 +15,7 @@ def format_one_ip(ip, required_fields=DEFAULT_FIELDS):
 
 
 def get_ips_info_using_api(ips, required_fields=DEFAULT_FIELDS, max_batch=MAX_API_BATCH_SIZE):
+    print('Getting location info for {} IP addresses'.format(len(ips)))
     data = [format_one_ip(ip, required_fields) for ip in ips]
 
     all_responses = []
@@ -23,3 +25,10 @@ def get_ips_info_using_api(ips, required_fields=DEFAULT_FIELDS, max_batch=MAX_AP
             all_responses += requests.post(API_URL, json=sublist).json()
             pbar.update(len(sublist))
     return all_responses
+
+
+def get_all_coordinates():
+    ips = get_ips_list()
+    coordinates = get_ips_info_using_api(ips, ['lon', 'lat'])
+
+    return coordinates
--- Python/ip_locations/ip_map.py
+++ Python/ip_locations/ip_map.py
@@ -1,8 +1,4 @@
 from Python.ip_locations.ip_location_tools import get_ips_info_using_api
-from Python.tools.data_getter import get_ips_list
 
 
-def get_all_coordinates():
-    ips = get_ips_list()
-    print('Getting location info for {} IP addresses'.format(len(ips)))
-    return get_ips_info_using_api(ips, ['lon', 'lat'])
+
