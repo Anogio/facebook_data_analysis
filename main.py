72c0f06e513fde93d239076a6199b415c39af111
Anogio <antoine.ogier@gmail.com>
2018-12-15 17:38:41 +0100

    Update main to reflect changes

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import operator
-
-from Python.conversation_analysis.activity_graphs import top_people_graph, get_messages_dataframe, add_conversation_activity_ratio
+from Python.conversation_analysis.activity_graphs import top_people_graph
+from Python.conversation_analysis.message_handling import get_messages_with_post_treatment
+from Python.ip_locations_map.ip_location_tools import get_all_coordinates
+from Python.ip_locations_map.ip_map import make_map
 from Python.tools.data_getter import get_conversations
 
 MY_NAME = 'Antoine Ogier'
 
-# coordinates = get_all_coordinates()
-# make_map(coordinates)
+# IP location map
+coordinates = get_all_coordinates()
+make_map(coordinates)
 
-conversations = get_conversations()
-messages_df = get_messages_dataframe(conversations)
-messages_df = add_conversation_activity_ratio(messages_df)
+# Conversation analysis
+## Boilerplate to get messages info
+messages_df = get_messages_with_post_treatment()
+## Activity graphs
 top_people_graph(messages_df, MY_NAME, 30)
-
