f12854798013ba06e4701cc345ff4f1bf6b54214
Anogio <antoine.ogier@gmail.com>
2018-12-15 20:19:16 +0100

    Add single person graph

--- Python/conversation_analysis/activity_graphs.py
+++ Python/conversation_analysis/activity_graphs.py
@@ -19,7 +19,22 @@ def save_graph(file_name):
     return decorator
 
 
-@save_graph('top_people.pdf')
+def save_graphs(file_names):
+    def decorator(wrapped):
+        def decorated(*args, **kwargs):
+            res = wrapped(*args, **kwargs)
+            for i, file_name in enumerate(file_names):
+                file_path = resolve_path(OUTPUT_FOLDER, file_name)
+                fig = res[i].get_figure()
+                fig.savefig(file_path, bbox_inches='tight')
+            return res
+
+        return decorated
+
+    return decorator
+
+
+@save_graph('top_people')
 def top_people_graph(messages_df, conversations_df, my_name, top_n):
     messages_df = pd.merge(messages_df, conversations_df, on=messages_cols.conversation, how='left')
 
@@ -34,8 +49,29 @@ def top_people_graph(messages_df, conversations_df, my_name, top_n):
 
 @save_graph('total_messages')
 def all_messages_over_time(messages_df, interval):
+    messages_df = messages_df.copy()
     messages_df['one'] = 1
-    rolling_n_messages = messages_df.set_index(messages_cols.date).sort_index()['one'].rolling(interval).sum().rename(
-        'n_messages_per_week').reset_index()
 
-    return rolling_n_messages.plot(x='date', y='n_messages_per_week')
+    amount_column_name = 'n_messages_per_' + interval
+    rolling_n_messages = messages_df.set_index(messages_cols.date).sort_index()['one'].rolling(interval).sum().rename(
+        amount_column_name).reset_index()
+
+    return rolling_n_messages.plot(x='date', y=amount_column_name)
+
+
+@save_graphs(['single_person_pie', 'single_person_activity'])
+def one_person_graphs(messages_df, name, interval='7d', top_n_pie=10):
+    person_df = messages_df[messages_df[messages_cols.sender] == name].copy()
+
+    messages_by_conversation = person_df.groupby(messages_cols.conversation)[messages_cols.sender].count()
+    top_messages_by_conversation = messages_by_conversation.sort_values().iloc[-top_n_pie:]
+    pie = pd.DataFrame(top_messages_by_conversation.rename('n_messages')).plot(
+        kind='pie', y='n_messages', title='{} (top {} conversations)'.format(name, top_n_pie), legend=False
+    )
+
+    amount_column_name = 'n_messages_per_' + interval
+    person_df['one'] = 1
+    line = person_df.set_index(messages_cols.date).sort_index()['one'].rolling(interval).sum().rename(
+        amount_column_name).reset_index().plot(x='date', y=amount_column_name, title=name)
+
+    return pie, line
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 import os
-from Python.conversation_analysis.activity_graphs import top_people_graph, all_messages_over_time
+from Python.conversation_analysis.activity_graphs import top_people_graph, all_messages_over_time, one_person_graphs
 from Python.conversation_analysis.message_handling import get_messages_with_post_treatment
 from Python.ip_locations_map.ip_location_tools import get_all_coordinates
 from Python.ip_locations_map.ip_map import make_map
@@ -21,5 +21,8 @@ top_people_graph(messages_df, conversation_stat_df, MY_NAME, top_n=30)
 print('Generating rolling messages_graph...')
 all_messages_over_time(messages_df, '7d')
 
+print('Generating single person graphs')
+detail_on = 'Dimitri Keriven Serpollet'
+one_person_graphs(messages_df, detail_on)
 print()
 print('Analysis completed in {time:.2f}s'.format(time=time.time() - t0))
