bf427f3b22b248be8a5801538a1e2c9323c59285
Anogio <antoine.ogier@gmail.com>
2018-12-15 18:30:13 +0100

    Cleanup

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from Python.conversation_analysis.activity_graphs import top_people_graph
 from Python.conversation_analysis.message_handling import get_messages_with_post_treatment
 from Python.ip_locations_map.ip_location_tools import get_all_coordinates
 from Python.ip_locations_map.ip_map import make_map
-from Python.tools.data_getter import get_conversations
 
 MY_NAME = 'Antoine Ogier'
 
