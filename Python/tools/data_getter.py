e4d33a1e9d2462a2bd74d6557cb99ad356cf1f5d
Anogio <antoine.ogier@gmail.com>
2018-12-15 10:11:11 +0100

    Make a function that gets all conversations

--- Python/tools/data_getter.py
+++ Python/tools/data_getter.py
@@ -1,8 +1,10 @@
 import json
+import os
 
 from Python.tools.helpers import resolve_path
 
 DATA_FOLDER = 'Data/'
+CONVERSATIONS_FOLDER = 'messages/'
 
 IP_FILE = 'security_and_login_information/used_ip_addresses.json'
 
@@ -15,3 +17,15 @@ def get_file(file):
 def get_ips_list():
     ips = get_file(IP_FILE)
     return [entry['ip'] for entry in ips['used_ip_address']]
+
+
+def get_conversations():
+    conversations = []
+    for path, subdirs, files in os.walk(resolve_path(DATA_FOLDER, CONVERSATIONS_FOLDER)):
+        for name in files:
+            if name == 'message.json':
+                with open(os.path.join(path, name), 'r' ) as file:
+                    conversations.append(json.load(file))
+    total_mesages = sum([len(conversation['messages']) for conversation in conversations])
+    print('Retrieved {} conversations with a total of {} messages'.format(len(conversations), total_mesages))
+    return conversations
