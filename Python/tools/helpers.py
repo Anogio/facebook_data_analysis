9827990080a830d85726909811311002f9513a9d
Anogio <antoine.ogier@gmail.com>
2018-12-15 10:59:40 +0100

    Method to get tile from timestamp

--- Python/tools/helpers.py
+++ Python/tools/helpers.py
@@ -1,4 +1,7 @@
 import os
+from datetime import datetime
+
+from dateutil import tz
 
 _PROJECT_ROOT_DIR = os.path.join(os.path.dirname(__file__), '../../')
 
@@ -21,3 +24,14 @@ def generate_sublists(list_to_split, max_sublist_size):
         for end in breaks[1:]:
             yield list_to_split[start:end]
             start = end
+
+
+def timestamp_to_local_date(timestamp_ms):
+    from_zone = tz.gettz('UTC')
+    to_zone = tz.tzlocal()
+
+    utc = datetime.utcfromtimestamp(timestamp_ms / 1000)
+    utc = utc.replace(tzinfo=from_zone)
+    local = utc.astimezone(to_zone)
+
+    return local
