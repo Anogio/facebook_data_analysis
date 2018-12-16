bbf2e7ef6465fcfa4ffaae437dba0d83a6164d99
Anogio <antoine.ogier@gmail.com>
2018-12-16 18:45:05 +0100

    Compute friends graph

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 import os
 from Python.conversation_analysis.activity_graphs import top_people_graph, all_messages_over_time, one_person_graphs
+from Python.conversation_analysis.friends_network import get_people_distances, get_projection_coordinates, friends_plot, \
+    friends_network
 from Python.conversation_analysis.message_handling import get_messages_with_post_treatment
 from Python.ip_locations_map.ip_location_tools import get_all_coordinates
 from Python.ip_locations_map.ip_map import make_map
@@ -22,7 +24,13 @@ print('Generating messages by week graph...')
 # all_messages_over_time(messages_df, '7d')
 
 print('Generating single person graphs')
-detail_on = 'Dimitri Lozeve'
+detail_on = 'Teven Le Scao'
 one_person_graphs(messages_df, detail_on)
 print()
+
+print('Generating co-occurrence graph')
+distances = get_people_distances(messages_df, conversation_stat_df)
+coordinates = get_projection_coordinates(distances)
+friends_plot(coordinates)
+friends_network(distances, 0.5)
 print('Analysis completed in {time:.2f}s'.format(time=time.time() - t0))
